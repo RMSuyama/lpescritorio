a1f1d81802ba9b577f8d30adb348740d8e551e59
RMSuyama <rafaelmoreirasuyama@gmail.com>
2023-06-24 12:26:11 -0300

    Revert "Revert "agora vai, encontrei o erro tava importando o próprio componente""
    This reverts commit 9bed24984724a740023d2919e3ae878f205ce9e9.

--- src/components/pages/Contato.js
+++ src/components/pages/Contato.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Navbar from '../common/Navbar/Navbar';
 import Painel from '../views/painel';
-
+import Footer from '../common/Footer/Footer'
 
 const Contato = () => {
   return (
     <div>
 
       <Navbar />  
-      <Contato />    
       <div>contato</div>
       <Painel />
+      <Footer />
+
 
     </div>
   );
--- src/components/pages/Vagas.js
+++ src/components/pages/Vagas.js
@@ -11,7 +11,7 @@ const Vagas = () => {
         <div className="card-body">
           <h5 className="card-title">Oportunidades de Carreira</h5>
           <p className="card-text">Confira as vagas disponíveis em nosso escritório e envie seu currículo para análise.</p>
-          <a href="#" className="btn btn-primary">Ver Vagas</a>
+          <a href="" className="btn btn-primary">Ver Vagas</a>
         </div>
       </div>
       <Footer />
